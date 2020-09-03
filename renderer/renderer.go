d794699f28f818057db8fe07e6ae6dd7bd828aa2
Jansen Price <sumpygump@gmail.com>
2020-09-02 20:13:41 -0500

    Correct linting errors

--- renderer/renderer.go
+++ renderer/renderer.go
@@ -269,7 +269,10 @@ func convertRegularGemini(s string, numLinks, width int, proxied bool) (string, 
 }
 
 func stripANSI(s string) string {
-	re := regexp.MustCompile("[\u001B\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))")
+	ansi := "[\u001B\u009B][[\\]()#;?]*" +
+		"(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007)|" +
+		"(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))"
+	re := regexp.MustCompile(ansi)
 	return re.ReplaceAllString(s, "")
 }
 

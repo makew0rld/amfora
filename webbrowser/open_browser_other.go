4a71b691eaee6e9fa70dff54c8b8f56a493eeb55
Timur Ismagilov <tims102rus@gmail.com>
2020-10-02 02:06:50 +0500

    Implement a more robust algorithm for opening web links on unices (#93)
    Co-authored-by: makeworld <25111343+makeworld-the-better-one@users.noreply.github.com>

--- webbrowser/open_browser_other.go
+++ webbrowser/open_browser_other.go
@@ -4,6 +4,7 @@ package webbrowser
 
 import "fmt"
 
+// Open opens `url` in default system browser, but not on this OS.
 func Open(url string) (string, error) {
 	return "", fmt.Errorf("unsupported OS for default HTTP handling. Set a command in the config")
 }

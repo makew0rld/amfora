2153e06207ccb92f7763d9062b80ace4813593a7
makeworld <makeworld@protonmail.com>
2024-12-18 19:14:10 -0500

    go fmt

--- display/display.go
+++ display/display.go
@@ -147,9 +147,9 @@ func Init(version, commit, builtBy string) {
 		// Reset func to set the bottomBar back to what it was before
 		// Use for errors.
 		reset := func() {
-      if searchMode {
-        resetSearch()
-      }
+			if searchMode {
+				resetSearch()
+			}
 			bottomBar.SetLabel("")
 			tabs[tab].applyAll()
 			App.SetFocus(tabs[tab].view)
@@ -159,61 +159,61 @@ func Init(version, commit, builtBy string) {
 		switch key {
 		case tcell.KeyEnter:
 
-      if searchMode {
-        // Escape the search string to not find regexp symbols
-        searchString = regexp.QuoteMeta(bottomBar.GetText())
+			if searchMode {
+				// Escape the search string to not find regexp symbols
+				searchString = regexp.QuoteMeta(bottomBar.GetText())
 
-        if strings.TrimSpace(searchString) == "" {
-          // Ignore
-          reset()
-          return
-        }
+				if strings.TrimSpace(searchString) == "" {
+					// Ignore
+					reset()
+					return
+				}
 
-        if tabs[tab].mode != tabModeSearch {
-          originalText = tabs[curTab].view.GetBytes(false)
-        }
-        tabs[tab].mode = tabModeSearch
+				if tabs[tab].mode != tabModeSearch {
+					originalText = tabs[curTab].view.GetBytes(false)
+				}
+				tabs[tab].mode = tabModeSearch
 
-        // find all positions of the search string
-        searchRegex := regexp.MustCompile(searchString)
-        searchIdx := searchRegex.FindAllIndex(originalText, -1)
+				// find all positions of the search string
+				searchRegex := regexp.MustCompile(searchString)
+				searchIdx := searchRegex.FindAllIndex(originalText, -1)
 
-        // find all positions of tags
-        tagsIdx := tagsRegex.FindAllIndex(originalText, -1)
+				// find all positions of tags
+				tagsIdx := tagsRegex.FindAllIndex(originalText, -1)
 
-        text := make([]byte, 0)
-        matches = 0
-        lastMatch := 0
+				text := make([]byte, 0)
+				matches = 0
+				lastMatch := 0
 
-        // loops through all occurrences and check if they
-        // discard if they lie within tags.
-        // []byte text is build from the original text buffer
-        // with the actual search strings replaced by tagged regions
-        // to highlight.
-        for i, match := range searchIdx {
-          for _, tag := range tagsIdx {
-            if match[0] >= tag[0] && match[1] <= tag[1] {
-              break
-            }
-          }
+				// loops through all occurrences and check if they
+				// discard if they lie within tags.
+				// []byte text is build from the original text buffer
+				// with the actual search strings replaced by tagged regions
+				// to highlight.
+				for i, match := range searchIdx {
+					for _, tag := range tagsIdx {
+						if match[0] >= tag[0] && match[1] <= tag[1] {
+							break
+						}
+					}
 
-          matches++
-          text = append(text, originalText[lastMatch:match[0]]...)
-          replacement := []byte(fmt.Sprint(`["search-`, i, `"]`, searchString, `[""]`))
-          text = append(text, replacement...)
-          lastMatch = match[0] + len(searchString)
-        }
-        text = append(text, originalText[lastMatch:]...)
+					matches++
+					text = append(text, originalText[lastMatch:match[0]]...)
+					replacement := []byte(fmt.Sprint(`["search-`, i, `"]`, searchString, `[""]`))
+					text = append(text, replacement...)
+					lastMatch = match[0] + len(searchString)
+				}
+				text = append(text, originalText[lastMatch:]...)
 
-        tabs[curTab].view.SetBytes(text)
+				tabs[curTab].view.SetBytes(text)
 
-        curMatch = 0
-        tabs[curTab].view.Highlight(fmt.Sprint("search-", "0"))
-        tabs[curTab].view.ScrollToHighlight()
-        App.SetFocus(tabs[tab].view)
+				curMatch = 0
+				tabs[curTab].view.Highlight(fmt.Sprint("search-", "0"))
+				tabs[curTab].view.ScrollToHighlight()
+				App.SetFocus(tabs[tab].view)
 
-        return
-      }
+				return
+			}
 
 			// Figure out whether it's a URL, link number, or search
 			// And send out a request
@@ -321,7 +321,6 @@ func Init(version, commit, builtBy string) {
 		// Other potential keys are Tab and Backtab, they are ignored
 	})
 
-
 	// Render the default new tab content ONCE and store it for later
 	// This code is repeated in Reload()
 	newTabContent := getNewTabContent()
@@ -392,7 +391,7 @@ func Init(version, commit, builtBy string) {
 				return nil
 			case config.CmdInvalid:
 				if event.Key() == tcell.KeyEsc {
-          resetSearch()
+					resetSearch()
 					return nil
 				}
 			}
@@ -417,10 +416,10 @@ func Init(version, commit, builtBy string) {
 				App.SetFocus(bottomBar)
 				return nil
 			case config.CmdSearch:
-        bottomBar.SetLabel("[::b]Search: [::-]")
-        bottomBarText = bottomBar.GetText()
+				bottomBar.SetLabel("[::b]Search: [::-]")
+				bottomBarText = bottomBar.GetText()
 				bottomBar.SetText("")
-        searchMode = true
+				searchMode = true
 				App.SetFocus(bottomBar)
 				return nil
 			case config.CmdEdit:
@@ -681,9 +680,9 @@ func NumTabs() int {
 }
 
 func resetSearch() {
-  tabs[curTab].view.SetBytes(originalText)
-  tabs[curTab].mode = tabModeDone
-  searchMode = false
-  bottomBar.SetLabel("")
-  bottomBar.SetText(bottomBarText)
+	tabs[curTab].view.SetBytes(originalText)
+	tabs[curTab].mode = tabModeDone
+	searchMode = false
+	bottomBar.SetLabel("")
+	bottomBar.SetText(bottomBarText)
 }
